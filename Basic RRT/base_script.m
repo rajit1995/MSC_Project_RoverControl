02cb796ac12f882e3b62bfc338a60a8784e37a03
Rajit Roy <83898887+rajit1995@users.noreply.github.com>
2023-10-17 12:45:55 +0100

    Add files via upload

--- Basic RRT/base_script.m
+++ Basic RRT/base_script.m
@@ -1,5 +1,6 @@
 clc;
 clearvars;
+warning('off','MATLAB:polyshape:repairedBySimplify')
 rrtvariables;
 load("FieldConst.mat");
 Dimensions.Length = Length;
@@ -19,7 +20,7 @@ tic;
 RRTState = RRTBASIC(RRTState);
 % RRTState = getpath(RRTState);
 elapsedTime = toc;
-filename = ['RRT' num2str(i) '.jpg'];
+filename = ['RRT' num2str(i) '.fig'];
 saveas(1,filename)
 Dist(i) = RRTState.Final.dist_total;
 Iterations(i) = RRTState.Final.Iterations;
@@ -28,7 +29,7 @@ tic;
 Rover = roverinit(RRTState);
 Rover = losapnav(Rover);
 elapsedTime = toc;
-filename = ['RRT_RoverLoS' num2str(i) '.jpg'];
+filename = ['RRT_RoverLoS' num2str(i) '.fig'];
 saveas(1,filename)
 time2(i) = elapsedTime;
 disp(['Iteration Completed:',num2str(i)])
@@ -43,12 +44,13 @@ Avg_time1 = mean(time1);
 Avg_time2 = mean(time2);
 Avg_dist2 = mean(Rover.dist_total1);
 %%
+clc;
 disp(['Average Iterations per run:',num2str(Avg_iterations)])
 disp(['Average Distance of the Path:',num2str(Avg_distance)])
 disp(['Average time taken :',num2str(Avg_time1)])
 disp(['Time taken by the Rover:',num2str(Avg_time2)])
 disp(['Distance covered by the Rover:',num2str(Avg_dist2)])
 %%
-openfig('APF_RRT_STAR1.fig')
-openfig('APF_RRT_STAR_RoverLoS1.fig')
+openfig('RRT1.fig');
+openfig('RRT_RoverLoS1.fig');
 
